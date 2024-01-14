93b9a1fc3f9c926506e9692be3395927163e553e
jeremiah <jermilive@gmail.com>
2024-01-14 11:23:36 -0600

    js update

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Define HTML Elements
+
+const board = document.getElementById('game-board');
+
+// Draw game variables
+let snake = [
+  {
+    x: 10,
+    y: 10,
+  },
+];
+
+// Draw game map and food
+
+function draw() {
+  board.innerHTML = '';
+  drawSnake();
+}
+
+// Draw snake
+function drawSnake() {}
