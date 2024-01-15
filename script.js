ecbe06d8328cfb31bab4d6ebe12e2c772e7415d6
Jeremiah Bates <jermilive@gmail.com>
2024-01-15 16:01:12 -0600

    update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const board = document.getElementById('game-board');
 
 // Draw game variables
+const gridSize = 20;
 let snake = [
   {
     x: 10,
@@ -10,11 +11,14 @@ let snake = [
   },
 ];
 
+let food = gennerateFood();
+
 // Draw game map and food
 
 function draw() {
   board.innerHTML = '';
   drawSnake();
+  drawFood();
 }
 
 // Draw snake
@@ -22,6 +26,7 @@ function drawSnake() {
   snake.forEach((segment) => {
     const snakeElement = createGameElement('div', 'snake');
     setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
   });
 }
 
@@ -29,10 +34,34 @@ function drawSnake() {
 function createGameElement(tag, className) {
   const element = document.createElement(tag);
   element.className = className;
+  return element;
 }
 
-// Set position of the snake or food
+// Set position of the snake or food cube/div
 function setPosition(element, position) {
   element.style.gridColumn = position.x;
   element.style.gridRow = position.y;
 }
+
+// Testing draw function
+draw();
+
+// draw food function
+function drawFood() {
+  const foodElement = createGameElement('div', 'food');
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+// Generate Food
+function gennerateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+// Moving the snake
+function move() {
+  const head = 
+}
+
